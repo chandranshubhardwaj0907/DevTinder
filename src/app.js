02baf00281d6fc13f83f23702e715073d3aecf39
chandranshu <chandrashubhardwaj68@gmail.com>
2024-12-25 17:35:29 +0530

    apis

--- src/app.js
+++ src/app.js
@@ -5,11 +5,10 @@ const User = require("./models/user");
 
 app.use(express.json());
 
-
 app.post("/signup", async (req, res) => {
-  console.log(req.body)
-  const user = new User(req.body)
- 
+  console.log(req.body);
+  const user = new User(req.body);
+
   try {
     await user.save();
     res.send("user added");
@@ -17,7 +16,33 @@ app.post("/signup", async (req, res) => {
     res.status(404).send("error");
   }
 });
-
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.send(400).send("somthing went wrong");
+  }
+});
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("user deleted successfully");
+  } catch (err) {
+    console.log("something went wrong");
+  }
+});
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+     await User.findByIdAndUpdate(userId, data,{returnDocument : "before"});
+    res.send("user updated successfully");
+  } catch (err) {
+    res.end("something went wrong");
+  }
+});
 connectdb()
   .then(() => {
     console.log("Connection established");
